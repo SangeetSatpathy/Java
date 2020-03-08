9ffcedc40a6abef4d76e3a86c6c7ca204a40b298
SangeetSatpathy <59673667+SangeetSatpathy@users.noreply.github.com>
2020-03-07 18:42:26 -0800

    Add files via upload

---
+++ helloworld.java
@@ -0,0 +1,4 @@
+
+public static void Main (String[] args){
+	System.out.println("hello world in JAVA")
+}
---
+++ usacoproblem1.java
@@ -0,0 +1,5 @@
+;//greater than or equal to 1, less than or equal to 20
+
+//k is greater than or equal to 1 and less than or equal to 10
+
+public static void random(int n, int k)
