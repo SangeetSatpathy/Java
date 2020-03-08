5c02213707302642c4f6110178373ef0c8c79039
SangeetSatpathy <59673667+SangeetSatpathy@users.noreply.github.com>
2020-03-07 18:41:05 -0800

    Add files via upload

---
+++ JavaProjects/Shinobi/Ninja_def.java
@@ -0,0 +1,9 @@
+abstract class Ninja_def {
+    String special_jutsu;
+    int chakra_levels;
+    int xp;
+    String rank;//Academy, gennin, chunnin, jonin, sannin, kage
+    
+    abstract void train(int hours);
+    
+}
